a84593837500a6ab3fc7027928e34ebf812b3696
niranjanmaradapu <85561667+niranjanmaradapu@users.noreply.github.com>
2021-08-19 11:40:17 +0530

    Delete CustomerDetails directory

--- CustomerDetails/src/main/java/com/customer/CustomerDetails/common/Status.java
+++
@@ -1,9 +0,0 @@
-package com.customer.CustomerDetails.common;
-
-public class Status {
-
-	public static final String CUSTOMER = "customer";
-	public static final String SAVE_CUSTOMERDETAILS = "savecustomerdetails";
-	public static final String GET_CUSTOMERDETAILS_BY_MOBILENUMBER = "getcustomerdetailsbymobilenuber";
-
-}
