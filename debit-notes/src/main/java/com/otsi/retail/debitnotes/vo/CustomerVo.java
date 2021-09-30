ee8b935c7218fd94ded5340de8c602d1fb1af8c9
vasavi.yakkali <vasavi.yakkali@otsi.co.in>
2021-09-30 11:33:40 +0530

    changed field in config class

--- debit-notes/src/main/java/com/otsi/retail/debitnotes/vo/CustomerVo.java
+++ debit-notes/src/main/java/com/otsi/retail/debitnotes/vo/CustomerVo.java
@@ -1,18 +1,17 @@
 package com.otsi.retail.debitnotes.vo;
 
-import java.time.LocalDate;
-
+import java.util.Date;
 import org.springframework.format.annotation.DateTimeFormat;
-
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+
 @Data
 public class CustomerVo {
 
 	private Long customerId;
 
 	private String name;
+	
+	private String email;
 
 	private String mobileNumber;
 
@@ -21,7 +20,7 @@ public class CustomerVo {
 	private String address;
 
 	@DateTimeFormat(pattern = "MM/dd/yyyy")
-	private LocalDate dob;
+	private Date dob;
 
 	private String gender;
 
