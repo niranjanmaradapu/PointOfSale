ce11979dc13d679de702d2af286c0e1df8fb6cb5
chandravengalarao <chandra.vengalarao@otsi.co.in>
2021-12-23 19:05:09 +0530

    Profiles and Log4j2 implemented

--- promo-exchange/src/main/java/com/otsi/retail/promoexchange/controller/PromoExchangeController.java
+++ promo-exchange/src/main/java/com/otsi/retail/promoexchange/controller/PromoExchangeController.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 import javax.validation.Valid;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -31,7 +31,9 @@ import com.otsi.retail.promoexchange.vo.PromoExchangeVo;
 @RequestMapping(CommonRequestMappings.PROMO_ITEM_EXCHANGE)
 public class PromoExchangeController {
 
-	private Logger log = LoggerFactory.getLogger(PromoExchangeController.class);
+	//private Logger log = LoggerFactory.getLogger(PromoExchangeController.class);
+	
+	  private Logger log=LogManager.getLogger(PromoExchangeController.class);
 
 	@Autowired
 	private PromoExchangeService promoExchangeService;
