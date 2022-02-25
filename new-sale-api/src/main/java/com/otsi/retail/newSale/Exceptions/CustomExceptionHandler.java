d0906665af5401d99b59128ecdbae0d1b21268fa
manideepTaninkiOtsi <manideep.taninki@otsi.co.in>
2022-02-25 13:58:32 +0530

    added PrematureCloseException and invokeIoexception

--- new-sale-api/src/main/java/com/otsi/retail/newSale/Exceptions/CustomExceptionHandler.java
+++ new-sale-api/src/main/java/com/otsi/retail/newSale/Exceptions/CustomExceptionHandler.java
@@ -11,6 +11,9 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 import com.otsi.retail.newSale.errors.ErrorResponse;
 
+import io.netty.channel.unix.Errors.NativeIoException;
+import reactor.netty.http.client.PrematureCloseException;
+
 @ControllerAdvice
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 
@@ -49,6 +52,16 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 		ErrorResponse<?> error = new ErrorResponse<>(405, "Invalid Input");
 		log.error("error response is:" + error);
 		return new ResponseEntity<Object>(error, HttpStatus.BAD_REQUEST);
+	}
+	@ExceptionHandler(value = PrematureCloseException.class)
+	public ResponseEntity<Object> handlePrematureCloseException(PrematureCloseException prematureCloseException) {
+		ErrorResponse<?> error = new ErrorResponse<>(500, prematureCloseException.getMessage());
+		return new ResponseEntity<Object>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	@ExceptionHandler(value = NativeIoException.class)
+	public ResponseEntity<Object> handleNativeIoException(NativeIoException nativeIoException) {
+		ErrorResponse<?> error = new ErrorResponse<>(500, nativeIoException.getMessage());
+		return new ResponseEntity<Object>(error, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
 }
